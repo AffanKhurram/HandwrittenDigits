596ef60ab9b80a776dfc1f3242d9a4b24a6e5396
Affan Khurram <20666920+AffanKhurram@users.noreply.github.com>
2019-04-04 14:43:11 -0500

    Add files via upload

--- ann.py
+++ ann.py
@@ -16,7 +16,19 @@ def computeLoss(y, y_hat):
 
 n_x = x.shape[0]
 m = x.shape[1]
+n_hidden = 64
 
-w = np.random.randn(n_x, 1) * 0.01
+w = np.random.randn(n_x, n_hidden) * 0.01
 b = np.zeros((1, 1))
 
+w2 = np.random.randn(64) * 0.01
+
+learning_rate = 1
+
+for i in range(2000):
+    # Input - Layer 2
+    z = np.matmul(W.T, x) + b  # Multiply the weights and inputs
+    a = sigmoid(z) # apply non-linearity to our multiplied values
+
+    # Layer 2 - Output
+    z2 = np.matmul()
