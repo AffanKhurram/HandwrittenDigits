c91fc3304398b3b2db5b0bb8b2d06cb8455038ed
Affan Khurram <20666920+AffanKhurram@users.noreply.github.com>
2019-04-08 14:42:12 -0500

    Add files via upload

--- ann.py
+++ ann.py
@@ -3,9 +3,9 @@ import numpy as np
 # Load the MNIST Dataset from sklearn
 from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784')
-x, y = mnist["data"], [float(i) for i in mnist["target"]]
+images, labels = mnist["data"], np.array([float(i) for i in mnist["target"]])
 
-x = x/255
+images = images/255
 
 def sigmoid(z):
     return 1/(np.exp(-z))
@@ -14,21 +14,19 @@ def computeLoss(y, y_hat):
     m = y.shape[1]
     l = -(1./m) * (np.sum(np.multiply(np.log(y_hat), y)) + np.sum(np.multiply(np.log(1-y_hat), (1-y))))
 
-n_x = x.shape[0]
-m = x.shape[1]
-n_hidden = 64
+n_x = images.shape[0]
+m = images.shape[1]
 
-w = np.random.randn(n_x, n_hidden) * 0.01
+w = np.random.randn(n_x, 1) * 0.01
 b = np.zeros((1, 1))
 
-w2 = np.random.randn(64) * 0.01
 
 learning_rate = 1
 
+print(np.matmul(w, images).shape)
+"""
 for i in range(2000):
-    # Input - Layer 2
-    z = np.matmul(W.T, x) + b  # Multiply the weights and inputs
-    a = sigmoid(z) # apply non-linearity to our multiplied values
-
-    # Layer 2 - Output
-    z2 = np.matmul()
+    # Input - Output
+    z = np.matmul(w.T, x) + b  # Multiply the weights and inputs
+    a = sigmoid(z) # apply non-linearity to our multiplied value
+"""
